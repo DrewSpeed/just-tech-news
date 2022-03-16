38770932fa75edc75d6176945271d09c2eb4f6c3
Drew Speed <drewjspeed@gmail.com>
2022-03-16 13:32:11 -0500

    ...

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,11 +4,7 @@ const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log(req.session);
-
-
   console.log('======================');
-
   Post.findAll({
     attributes: [
       'id',
@@ -46,17 +42,8 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
+// get single post
 router.get('/post/:id', (req, res) => {
-
   Post.findOne({
     where: {
       id: req.params.id
@@ -89,10 +76,8 @@ router.get('/post/:id', (req, res) => {
         return;
       }
 
-      // serialize the data
       const post = dbPostData.get({ plain: true });
 
-      // pass data to template
       res.render('single-post', {
         post,
         loggedIn: req.session.loggedIn
@@ -102,6 +87,15 @@ router.get('/post/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
