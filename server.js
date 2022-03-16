2386f1d9bc07510e3940d78bb5b1e539df07deba
Drew Speed <drewjspeed@gmail.com>
2022-03-15 20:44:36 -0500

    login and logout

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-<<<<<<< .merge_file_NTDvKT
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-
 const app = express();
+const sequelize = require('./config/connection');
 const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-=======
 const path = require('path');
-const express = require('express');
 const exphbs = require('express-handlebars');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const sequelize = require('./config/connection');
-
+const session = require('express-session');
 const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -40,4 +32,3 @@ app.use(require('./controllers/'));
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
->>>>>>> .merge_file_zP0W3A
